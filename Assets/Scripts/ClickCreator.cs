4dde4855d61577a7352a75bc9ec382c212eddb73
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-03-06 01:26:15 +0900

    Initial Commit
    初期コミット

---
+++ Assets/Scripts/ClickCreator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickCreator : MonoBehaviour
+{
+    [SerializeField] protected GameObject CreateObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mPos = Input.mousePosition;
+            mPos = Camera.main.ScreenToWorldPoint(mPos);
+            mPos.z = 0f;
+            Instantiate(this.CreateObject, mPos, Quaternion.identity, this.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Power/ShockWave.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider))]
+public class ShockWave : MonoBehaviour
+{
+    [SerializeField] protected float Power = 10;
+    [SerializeField] protected float Size = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var rigid = this.GetParentRigidbody(other.gameObject);
+        if (rigid != null)
+        {
+            rigid.AddExplosionForce(this.Power, this.transform.position, this.Size);
+        }
+
+        /*
+        var dir = Vector3.ClampMagnitude(
+                other.transform.position - this.transform.position,
+                this.Power
+            );
+        */
+
+    }
+
+    protected Rigidbody GetParentRigidbody(GameObject gameObject, int depth = 99)
+    {
+        if (depth <= 0 || gameObject == null) return null;
+        var rigid = gameObject.GetComponent<Rigidbody>();
+        if (rigid != null)
+        {
+            return rigid;
+        }
+        return this.GetParentRigidbody(gameObject.transform.parent.gameObject, --depth);
+    }
+}
