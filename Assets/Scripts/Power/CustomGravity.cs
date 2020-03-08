56ffb65559714438ad755e20553af483464a8ada
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-03-08 21:07:09 +0900

    import DoTween
    ver 1.2.335

---
+++ Assets/Scripts/Power/CustomGravity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CustomGravity : MonoBehaviour
+{
+    public Vector3 GravityForce;    
+    
+    private new Rigidbody rigidbody;
+    private Vector3 force
+        => this.GravityForce * Physics.gravity.magnitude;
+
+
+    private void Start()
+    {
+        this.rigidbody = this.GetComponent<Rigidbody>();
+        this.rigidbody.useGravity = false;
+    }
+
+    private void FixedUpdate()
+    {
+        this.rigidbody.AddForce(this.force, ForceMode.Acceleration);
+    }
+}
