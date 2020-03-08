1f0e30a28a8bf22be6c10f118d227639c119aa96
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-03-08 22:59:58 +0900

    クソエルモをカメラ追尾するようになった

---
+++ Assets/Scripts/Chaser.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class Chaser : MonoBehaviour
+{
+
+    public Transform TargetObject;
+    public float DoTime;
+    public Vector3 PositionDiff;
+    protected Vector3 positionTo
+        => this.TargetObject != null ? this.TargetObject.position + this.PositionDiff : this.transform.position;
+    protected Vector3 positionToDiff
+        => this.TargetObject != null ? this.TargetObject.position - this.transform.position + this.PositionDiff : this.transform.position;
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.DoTime > 0)
+        {
+            this.transform.DOMove(this.positionToDiff, this.DoTime);
+        }
+        else
+        {
+            this.transform.DOKill();
+            this.transform.position = this.positionTo;
+        }
+    }
+}
---
+++ Assets/Scripts/Power/SpeedClamp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpeedClamp : MonoBehaviour
+{
+    public float MaxSpeed;
+    private new Rigidbody rigidbody;
+    private void Start()
+    {
+        this.rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var speed = this.rigidbody.velocity.magnitude;
+
+        if (speed > this.MaxSpeed)
+        {
+            this.rigidbody.velocity = this.MaxSpeed * this.rigidbody.velocity.normalized;
+        }
+    }
+}
