0de5aa0ec27276350776adc27e750f5c50b69306
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-03-26 00:47:52 +0900

    タイトルからテスト画面へ遷移処理を実装
    Game.Controllers.SceneTransister

---
+++ Assets/Scripts/Game/Controllers/StaticGameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Game.Controllers
+{
+    public class StaticGameController : MonoBehaviour
+    {
+
+    }
+
+}
