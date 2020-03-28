7c13eb092497012974a10ef14e46bfbb8d269ebf
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-03-29 01:54:17 +0900

    リザルト画面をとりあえずUIだけ実装
    できること(すべてはりぼて)
    ・スコア
       ・到達マップ(予定)
    ・獲得ポイント
    ・リトライボタン
    
    ・メニューに戻る
    ・ショップ
    ・共有

---
+++ Assets/Scripts/Game/Controllers/StageController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Game.Controllers
+{
+    public class StageController : MonoBehaviour
+    {
+        private bool isGaming;
+        protected void GameStart()
+        {
+
+        }
+        protected void GameOver()
+        {
+
+        }
+
+        protected void GameResult()
+        {
+
+        }
+
+        protected void GameReset()
+        {
+
+        }
+    }
+}
