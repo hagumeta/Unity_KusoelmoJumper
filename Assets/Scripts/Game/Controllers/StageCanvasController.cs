00e8182b1d0b39599cfd0bf82af778fc4097e286
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-04-05 03:04:48 +0900

    ステージUI用のコントローラの実装(StageCanvasController)

---
+++ Assets/Scripts/Game/Controllers/StageCanvasController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Game.Controllers
+{
+    public class StageCanvasController : MonoBehaviour
+    {
+        private StageController stageController;
+
+        [SerializeField] private TMPro.TMP_Text scoreText;
+
+        void Start()
+        {
+            this.stageController = GameObject.FindObjectOfType<StageController>();
+        }
+
+        public void RestartGame()
+        {
+            this.stageController.Restart();
+        }
+
+        private void Update()
+        {
+            this.RefreshView();
+        }
+
+        private void RefreshView()
+        {
+            var score = this.stageController.GameScore;
+            this.scoreText.text = score.Score.ToString();
+        }
+    }
+
+}
