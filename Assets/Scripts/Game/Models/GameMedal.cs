795d3c189895d08febeeb82b808e31cbf324968d
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-04-05 16:19:14 +0900

    ステージ中獲得メダル数のクラスを定義
    Game.Models.GameMedal

---
+++ Assets/Scripts/Game/Models/GameMedal.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Game.Models
+{
+    public class GameMedal
+    {
+        public int Medal { get; set; }
+
+        public GameMedal()
+        {
+            this.Medal = 0;
+        }
+    }
+}
