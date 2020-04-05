09251bb5f343dd4d9e4d5755f13a0ef612d39690
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-04-05 23:34:37 +0900

    ゲームスコアカウンタを用意．
    Game.GameScoreCounter

---
+++ Assets/Scripts/Game/GameScoreCounter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Game.Controllers
+{
+    public class GameScoreCounter : MonoBehaviour
+    {
+        [SerializeField] private StartPoint startPoint;
+        [SerializeField] private float ScoreRateFromDistance = 1f;
+
+        private StageController stageController;
+        private float distance;
+        private int score;
+        public int AdditionalScore;
+
+        private void Start()
+        {
+            this.stageController = FindObjectOfType<StageController>();
+        }
+
+        private void Update()
+        {
+            this.UpdateScore();
+        }
+
+        private void UpdateScore()
+        {
+            if (this.startPoint.GetActor != null)
+            {
+                this.distance = Vector3.Distance(this.startPoint.transform.position, this.startPoint.GetActor.transform.position);
+                this.score = Mathf.FloorToInt(this.AdditionalScore + this.distance * this.ScoreRateFromDistance);
+            }
+            this.stageController.GameScore.Score = this.score;
+        }
+    }
+}
--- Assets/Scripts/Game/StartPoint.cs
+++ Assets/Scripts/Game/StartPoint.cs
@@ -10,10 +10,12 @@ namespace Game
 
         [SerializeField] protected Actor.Actor Actor;
         [SerializeField] protected bool CreateOnStart;
+        public Actor.Actor GetActor { get; private set; }
 
         public void CreateActor()
         {
             var act = Instantiate(this.Actor.gameObject, this.transform.position, Quaternion.identity);
+            this.GetActor = act.GetComponent<Actor.Actor>();
         }
 
         private void Start()
