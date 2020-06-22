18a992d3aa767d8cfb19b9c6acbc3e593bf8a6e3
Hagujiro Hagumeta <y.yuki.game@gmail.com>
2020-06-23 00:24:06 +0900

    GodTouchアセットを導入
    https://github.com/okamura0510/GodTouch

---
+++ Assets/Assets_Plugins/GodTouch/Scripts/Demo.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace GodTouches
+{
+    public class Demo : MonoBehaviour
+    {
+        public Transform Move;
+
+        Vector3 startPos;
+
+        void Start()
+        {
+            startPos = Move.position;
+        }
+
+        void Update()
+        {
+            // タッチを検出して動かす
+            var phase = GodTouch.GetPhase();
+            if (phase == GodPhase.Began)
+            {
+                startPos = Move.position;
+            }
+            else if (phase == GodPhase.Moved)
+            {
+                Move.position = GodTouch.GetPosition();
+                //             Move.position += GodTouch.GetDeltaPosition(); 
+            }
+            else if (phase == GodPhase.Ended)
+            {
+                Move.position = startPos;
+            }
+        }
+    }
+}
